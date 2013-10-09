b5b4bdb7a8676f97a94ac194aea6506ec2a47b9a
steri <sr-richi@web.de>
2013-10-09 16:03:58 +0200

    Added a very simple xml-example.

---
+++ src/main/java/de/steri/springbatch/xml/processing/domain/Datei.java
@@ -0,0 +1,79 @@
+package de.steri.springbatch.xml.processing.domain;
+
+import java.util.List;
+
+public class Datei {
+
+	private String datum;
+	private String ersteller;
+	//private List<Person> personen;
+
+	public String getDatum() {
+		return datum;
+	}
+
+	public void setDatum(String datum) {
+		this.datum = datum;
+	}
+
+	public String getErsteller() {
+		return ersteller;
+	}
+
+	public void setErsteller(String ersteller) {
+		this.ersteller = ersteller;
+	}
+/*
+	public List<Person> getPersonen() {
+		return personen;
+	}
+
+	public void setPersonen(List<Person> personen) {
+		this.personen = personen;
+	}*/
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		result = prime * result
+				+ ((ersteller == null) ? 0 : ersteller.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Datei other = (Datei) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (ersteller == null) {
+			if (other.ersteller != null)
+				return false;
+		} else if (!ersteller.equals(other.ersteller))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Datei [datum=");
+		builder.append(datum);
+		builder.append(", ersteller=");
+		builder.append(ersteller);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	
+}
---
+++ src/main/java/de/steri/springbatch/xml/processing/domain/Person.java
@@ -0,0 +1,77 @@
+package de.steri.springbatch.xml.processing.domain;
+
+import java.util.List;
+
+public class Person {
+
+	private String name;
+	private String vorname;
+	private List<Adresse> adressen;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getVorname() {
+		return vorname;
+	}
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+	public List<Adresse> getAdressen() {
+		return adressen;
+	}
+	public void setAdressen(List<Adresse> adressen) {
+		this.adressen = adressen;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((adressen == null) ? 0 : adressen.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((vorname == null) ? 0 : vorname.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (adressen == null) {
+			if (other.adressen != null)
+				return false;
+		} else if (!adressen.equals(other.adressen))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (vorname == null) {
+			if (other.vorname != null)
+				return false;
+		} else if (!vorname.equals(other.vorname))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Person [name=");
+		builder.append(name);
+		builder.append(", vorname=");
+		builder.append(vorname);
+		builder.append(", adressen=");
+		builder.append(adressen);
+		builder.append("]");
+		return builder.toString();
+	}
+}
