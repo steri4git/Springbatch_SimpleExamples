00800dc272fbf775ce448eafbc26f4b1003073d4
steri4git <sr-richi@web.de>
2013-12-19 22:33:34 +0100

    test

--- src/main/java/de/steri/springbatch/complex/processing/exception/RetryableException.java
+++ src/main/java/de/steri/springbatch/complex/processing/exception/RetryableException.java
@@ -1,6 +1,13 @@
 package de.steri.springbatch.complex.processing.exception;
 
 public class RetryableException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
 	
 
 }
